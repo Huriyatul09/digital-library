e339d666f17c1d4e25e6d7bd1d36c945bbc2995d
jannahhuriyatul5@gmail.com <Huriyatul09>
2025-10-29 23:29:12 +0700

    Initial commit: Laravel backend & Vue frontend skeleton

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -3,23 +3,25 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker; // Impor class Faker
+use Illuminate\Support\Facades\Hash; // Impor Hash untuk password
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // Tugas
+        $this->call([
+            MemberSeeder::class,
+            AuthorSeeder::class,
+            PublisherSeeder::class,
+            BookSeeder::class,
         ]);
     }
 }
